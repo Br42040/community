dc98452e7faaec10e6f1f8f459e84733aa970458
Br42040 <br42040@gmail.com>
2024-03-13 17:48:36 -0400

    Update discussions.rb

--- .github/lib/discussions.rb
+++ .github/lib/discussions.rb
@@ -6,9 +6,8 @@ require "active_support/core_ext/numeric/time"
 Discussion = Struct.new(
   :id,
   :url,
-  :title
-) do
-  def self.all(owner: nil, repo: nil, category: nil)
+  :tit
+  .all(owner: nil, repo: nil, category: nil)
     return [] if owner.nil? || repo.nil? || category.nil?
 
     query = <<~QUERY
@@ -48,65 +47,49 @@ Discussion = Struct.new(
       }
     }
     QUERY
+ 
+  
+      .
 
-    cutoff_date = Time.now.advance(days: -60)
-    GitHub.new.post(graphql: query).map! { |r| r.dig('discussions', 'nodes') }
-      .flatten
-      .reject { |r| Date.parse(r["updatedAt"]).after?(cutoff_date) }
-      .select { |r| r.dig("labels", "nodes").map { |l| l["name"] }.include?("Question") }
-      .reject { |r| r["closed"] }
-      .reject { |r| r["locked"] }
-      .reject { |r| r.dig("comments", "totalCount") > 0 && Date.parse(r.dig("comments", "nodes", 0, "createdAt")).after?(cutoff_date) }
-      #.reject { |r| r.dig("labels", "nodes").map { |l| l["name"] }.include?("stale") }
-      .select { |r| r.dig("labels", "nodes").map { |l| l["name"] }.include?("stale") }
+      
+}
+")).after?(cutoff_date) 
+ }.include
       .map do |c|
         Discussion.new(
-          c["id"],
-          c["url"],
-          c["title"]
+    
+          
+        
         )
       end
   end
 
-  def add_comment(body: nil)
+  def add_comment(body:
     query = <<~QUERY
     mutation {
       addDiscussionComment(
         input: {
           body: "#{body}",
-          discussionId: "#{self.id}",
-          clientMutationId: "rubyGraphQL"
+          discussionId: 
+          clientMutationId: 
         }
       ) {
-        clientMutationId
-        comment {
-           id
-           body
+      
+    
+           
+           
         }
       }
     }
     QUERY
 
-    GitHub.new.mutate(graphql: query)
-  end
 
-  def add_label(label_id: nil)
-    return if label_id.nil?
 
-    query = <<~QUERY
-    mutation {
-      addLabelsToLabelable(
-        input: {
-          labelIds: ["#{label_id}"],
-          labelableId: "#{self.id}",
-          clientMutationId: "rubyGraphQL"
+
         }
-      ) {
-        clientMutationId
+      ) 
       }
     }
     QUERY
 
-    GitHub.new.mutate(graphql: query)
-  end
-end
+    GitHub.new.mutate(graphql:
